b9e827b57fd605e25fa454b03bc65fd325f9505a
aab <aabccd021@gmail.com>
2021-01-16 22:11:20 +0700

    Test if coverage decreases in main if pushed to dev branch (#9)
    * Implement hello world
    
    * Add prettier
    
    * deleted prettierrc
    
    * Add lint and prettier configuration
    
    * chore: add yarn-error.log to .gitignore
    
    * chore: add mocha test
    
    * chore: add istanbul coverage
    
    * chore: strict yarn and node engine
    
    * chore: report coverage json and text in a command
    
    * chore: dummy code change
    
    * chore: renamed type workflow(s)
    
    * chore: fixed github wordkflow node version
    
    * chore: add coverage file location for codecov workflow
    
    * chore: add name to main workflow
    
    * chore: removed lcov coverage from scripts
    
    * chore: update readme
    
    * chore: renamed CI workflow job name
    
    * chore: add editorconfig checker
    
    * chore: fixed tsconfig.json to follow editorconfig
    
    * chore: setup semantic-release
    
    * chore: update yarn lock for semantic release
    
    * chore: fix release workflow
    
    * chore: rename gh_token
    
    * chore(commitizen): add commitizen
    
    * chore(lint-staged): add lint-staged to project
    
    * chore(lint-staged): add prettier as dependency
    
    * chore(precommit-hooks): add editor config lint to pre-commit
    
    * chore(commitizen): add commitizen badge to readme
    
    * chore(test): add type test
    
    * chore(readme): add license badge to readme
    
    * chore(coverage): replace codecov with codeclimate
    
    * chore(readme): add code climate badge to readme
    
    * chore(gpg): gpg commit test
    
    * chore(index): check if coverage decreases

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export function helloWorld(x: number): number {
-  if (x > 100) return 100;
+  if (x > 100) return haroWarudo(x);
   return x + 1;
+}
+
+function haroWarudo(x: number): number {
+  return x + 2;
 }
 
 const result = helloWorld(2);
