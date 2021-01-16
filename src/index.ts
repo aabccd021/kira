35daaac3a28efca442f59b5a3291bb476a760792
aab <aabccd021@gmail.com>
2021-01-16 23:20:38 +0700

    Remove coverage upload from test.yml (#11)
    * Implement hello world
    
    * Add prettier
    
    * deleted prettierrc
    
    * Add lint and prettier configuration
    
    * chore: add yarn-error.log to .gitignore
    
    * chore: add mocha test
    
    * chore: add istanbul coverage
    
    * chore: strict yarn and node engine
    
    * chore: report coverage json and text in a command
    
    * chore: dummy code change
    
    * chore: renamed type workflow(s)
    
    * chore: fixed github wordkflow node version
    
    * chore: add coverage file location for codecov workflow
    
    * chore: add name to main workflow
    
    * chore: removed lcov coverage from scripts
    
    * chore: update readme
    
    * chore: renamed CI workflow job name
    
    * chore: add editorconfig checker
    
    * chore: fixed tsconfig.json to follow editorconfig
    
    * chore: setup semantic-release
    
    * chore: update yarn lock for semantic release
    
    * chore: fix release workflow
    
    * chore: rename gh_token
    
    * chore(commitizen): add commitizen
    
    * chore(lint-staged): add lint-staged to project
    
    * chore(lint-staged): add prettier as dependency
    
    * chore(precommit-hooks): add editor config lint to pre-commit
    
    * chore(commitizen): add commitizen badge to readme
    
    * chore(test): add type test
    
    * chore(readme): add license badge to readme
    
    * chore(coverage): replace codecov with codeclimate
    
    * chore(readme): add code climate badge to readme
    
    * chore(gpg): gpg commit test
    
    * chore(index): check if coverage decreases
    
    * ci(workflow): separated release job from test
    
    * chore(package.json): sort package json before commit
    
    * chore(package.json): sort package.json before commit
    
    * chore(json): just sort json manually
    
    * ci(workflow): removed coverage upload from non-main workflow
    
    * ci(main.yml): rename release.yml to main.yml
    
    * chore(test.yml): changed `yarn test:coverage` to `yarn test`
    
    * ci(test.yml): upload coverage on non-main workflow

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 export function helloWorld(x: number): number {
-  if (x > 100) return haroWarudo(x);
   return x + 1;
-}
-
-function haroWarudo(x: number): number {
-  return x + 2;
 }
 
 const result = helloWorld(2);
