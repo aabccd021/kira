2c5d822741e0f94a26178875d9b8b497000010a4
aabccd021 <aabccd021@gmail.com>
2021-02-02 17:48:57 +0700

    test(field.test): remove unused test

--- test/field.test.ts
+++
@@ -1,8 +0,0 @@
-import { Field } from '../src/field-type';
-
-describe('Fields', () => {
-  it('has type property', () => {
-    const field: Field = { type: 'boolean' };
-    field.type;
-  });
-});
